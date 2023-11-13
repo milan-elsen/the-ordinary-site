fa9833c51f0f837538332f4ef4bf08986a6728ea
Milan Elsen <me@users-first.de>
2023-11-13 16:06:39 +0100

    Fixed Dark Mode Toggle

--- site.js
+++ site.js
@@ -1,9 +1,16 @@
 function toggleDarkMode() {
     var bodyElement = document.body;
 
-    if (bodyElement.classList.contains("dark-theme")) {
-        bodyElement.classList.toggle("dark-theme");
-    } else {
+
+    const darkModePreference = window.matchMedia("(prefers-color-scheme: dark)");
+
+    if (darkModePreference.matches) {
+        bodyElement.classList.remove("dark-theme");
         bodyElement.classList.toggle("light-theme");
+    } else {
+        bodyElement.classList.remove("light-theme");
+        bodyElement.classList.toggle("dark-theme");
     };
+
+    console.log(bodyElement.classList);
 }
